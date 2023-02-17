263cc6c3707a3c9ee97911b0ba9bd0efb381025e
Parkerkuch <117104975+Parkerkuch@users.noreply.github.com>
2023-02-17 11:47:00 -0500

    This should fix it

--- ScribTest.java
+++ ScribTest.java
@@ -1,6 +1,6 @@
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+//import org.junit.Before;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
 
 public class ScribTest {
 
